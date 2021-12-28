25943f53611176a402a1402a4e94b1dc0c34471f
Ben P <przybyszewski.benjamin@gmail.com>
2021-12-28 15:26:47 -0600

    reformat script

--- 313/313-scoreboard.js
+++ 313/313-scoreboard.js
@@ -1,7 +1,7 @@
 var table;
 var round = 3;
 
-function addItems(){
+function addItems() {
 
     //Map the table to the table variable
     var table = document.getElementById("scoreboard");
@@ -18,23 +18,23 @@ function addItems(){
     // Insert the row for 
     var newCell = newRow.insertCell(0);
     newCell.innerHTML = round;
-    newCell.outerHTML = "<th class=\"round0\">" + round +"</th>";
+    newCell.outerHTML = "<th class=\"round0\">" + round + "</th>";
 
     //Loop to add all players scores to the table    
-    for (var i = 0; i < numberItems; i++){
+    for (var i = 0; i < numberItems; i++) {
         var score;
-        if (document.getElementsByClassName("scorebox")[i].value == ""){
+        if (document.getElementsByClassName("scorebox")[i].value == "") {
             score = 0;
         }
-        else{
+        else {
             score = parseInt(document.getElementsByClassName("scorebox")[i].value);
-            
+
         }
 
         playerScore.push(score);
 
         //Insert the value of the player's score for the round
-        newCell = newRow.insertCell(i+1);
+        newCell = newRow.insertCell(i + 1);
         newCell.innerHTML = score;
 
         //Reset the value of the text Boxes
@@ -50,9 +50,9 @@ function addItems(){
     //Execute the addscore function and passthrough the playerScore array
     addScore(playerScore);
 }
-function addScore(playerScore){
+function addScore(playerScore) {
     var playerTotal = [];
-    for (var i = 0; i < numberItems; i++){
+    for (var i = 0; i < numberItems; i++) {
 
         //Get the total variable from the innerhtml of the totalscore class
         var total = parseInt(document.getElementsByClassName("totalscore")[i].innerHTML.trim());
@@ -62,58 +62,35 @@ function addScore(playerScore){
 
         //Set the total HTML to the total
         document.getElementsByClassName("totalscore")[i].innerHTML = total;
-        
+
         //Add the total to the playertotal array
         playerTotal.push(total);
-        
     }
-    
+
     //Log the player total array
     console.log(playerTotal);
-    
+}
+
+function addPlayer() {
+    while (document.getElementById("ExamplePlayer")) {
+        players.deleteCell(-1)
+        total.deleteCell(-1)
+        add.deleteCell(-1)
     }
 
-    function addPlayer(){
-        while(document.getElementById("ExamplePlayer")){
-            players.deleteCell(-1)
-            total.deleteCell(-1)
-            add.deleteCell(-1)
-        }
+    var newPlayerName = document.getElementById("addPlayer").value;
+    var newPlayerCell = players.insertCell(-1);
+    newPlayerCell.innerHTML = newPlayerName;
 
-        var newPlayerName = document.getElementById("addPlayer").value;
-        var newPlayerCell = players.insertCell(-1);
-        newPlayerCell.innerHTML = newPlayerName;
+    var newTotalCell = total.insertCell(-1);
+    newTotalCell.outerHTML = "<td class=\"totalscore\">0</td>";
 
-        var newTotalCell = total.insertCell(-1);
-        newTotalCell.outerHTML = "<td class=\"totalscore\">0</td>";
-        
-        var newAddCell = add.insertCell(-1);
-        newAddCell.outerHTML = '<td><input type="text" id="1" placeholder="Score" class="scorebox"/></td>';
+    var newAddCell = add.insertCell(-1);
+    newAddCell.outerHTML = '<td><input type="text" id="1" placeholder="Score" class="scorebox"/></td>';
 
-        document.getElementById("addPlayer").value = "";
+    document.getElementById("addPlayer").value = "";
+}
 
-        
-        /* if(document.getElementById("ExamplePlayer")){
-            players.deleteCell(-1)
-            total.deleteCell(-1)
-            add.deleteCell(-1)
-        }
-        else{
-            var newPlayerName = document.getElementById("addPlayer").value;
-            var newPlayerCell = players.insertCell(-1);
-            newPlayerCell.innerHTML = newPlayerName;
-    
-            var newTotalCell = total.insertCell(-1);
-            newTotalCell.outerHTML = "<td class=\"totalscore\">0</td>";
-            
-            var newAddCell = add.insertCell(-1);
-            newAddCell.outerHTML = '<td><input type="text" id="1" placeholder="Score" class="scorebox"/></td>';
-    
-            document.getElementById("addPlayer").value = "";
-
-        } */
-    }
-
-    function debug(){
-        console.log(document.getElementsByClassName("scorebox")[1]);
+function debug() {
+    console.log(document.getElementsByClassName("scorebox")[1]);
 }
